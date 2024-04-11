9a86e96c5323a6fb2c1547cb08718e1540a7ad79
Dani <danixidev@gmail.com>
2024-04-11 13:46:27 +0200

    Create basic files

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dximenez <dximenez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 13:44:31 by dximenez          #+#    #+#             */
+/*   Updated: 2024/04/11 13:45:59 by dximenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
